77c37cb2375f8fb8d4660d2d11c82744fc638264
kisublee <yigi2021@gmail.com>
2022-01-19 19:53:15 -0500

    Update routes.

--- src/routing/Routing.js
+++ src/routing/Routing.js
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import MarsRover from "../components/mars/MarsRover";
 import Home from "../components/Home";
 const Routing = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/mars" element={<MarsRover />} />
     </Routes>
   );
 };
